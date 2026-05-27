41676b9527a168a5d2f40d0a7bee94b7d0deffad
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-05-27 20:53:12 +0600

    Fix formatting and add new lines in numpy_array.py

--- NumPy/numpy_array.py
+++ NumPy/numpy_array.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
-print(arr * 2) # multiply arr by 2 and print the result
-print(arr.mean()) # print the mean of arr
-print(arr.shape) # print arr size  
+
+print(arr * 2)   # multiply arr by 2 and print the result
+print(arr.mean())  # print the mean of arr
+print(arr.shape)   # print arr size  
