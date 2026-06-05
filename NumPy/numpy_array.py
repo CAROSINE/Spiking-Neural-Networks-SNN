390363c8c13800e3489a9cb3b225cb8f3beae250
Md.Moshiur Rahman Sajol <moshiur287rahman@gmail.com>
2026-06-05 20:56:24 +0600

    Add mean and shape calculations for NumPy array

--- NumPy/numpy_array.py
+++ NumPy/numpy_array.py
@@ -3,5 +3,7 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 
 print(arr * 2)   # multiply arr by 2 and print the result
+
 print(arr.mean())  # print the mean of arr
+
 print(arr.shape)   # print arr size  
